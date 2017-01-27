663483944298dd3049f037ba925a09de6b5c8660
Prohrust <mjaroslav@mail.ru>
2017-01-27 16:38:24 +0300

    Create RRRR.c

---
+++ KK/RRRR.c
@@ -0,0 +1,57 @@
+
+#include<stdio.h>
+
+// Test #2 @wi.runs_2 COMP-556 ReMoTe i
+int i = 0;
+
+
+typedef int *int_ptr;
+
+
+int x;
+
+
+enum days myDay;
+
+
+int max(int a, int b);
+
+
+int max(int a, int b)
+{
+
+};
+
+void (*process(int, void (*)(int)))(int) {
+
+};
+
+
+struct BBooks BBook1;
+
+
+struct Books {
+   char  title[50];
+   char  author[50];
+   char  subject[100];
+   int   book_id;
+} book;
+
+
+union MyUnion {
+   int i;
+   float f;
+   char str[20];
+}; 
+
+
+enum days {MONDAY, TUESDAY, WEDNESDAY};
+
+
+typedef struct {
+	unsigned int src;
+	unsigned int dst;
+	unsigned int type;
+  unsigned int crc;
+ 	unsigned int data[12];
+}Packet;
