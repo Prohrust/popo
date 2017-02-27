185cf579afe3ca1ba4cf9df4406dd5e8fe88e960
Prohrust <mjaroslav@mail.ru>
2017-02-27 12:20:53 +0300

    Update codeC.c

--- KK/codeC.c
+++ KK/codeC.c
@@ -1,7 +1,7 @@
 Single line:
 #define TRACE_SYSTEM sched
 
-// @wi COMPOT/COMP-553 link comment
+// @wi QAZ/QAZ-76 link comment
 static int fcmp_eq(struct sh_fpu_soft_struct *fregs, struct pt_regs *regs, int m, int n){return 0;}
 
 
@@ -9,11 +9,11 @@ Multi line:
 #define BOTH_PRmn(op,x) \
 FP_DECL_EX; if(FPSCR_PR) op(D,x,DRm,DRn); else op(S,x,FRm,FRn);
                
-// @wi COMP-553 link comment
+// @wi QAZ-76 link comment
 static int fcmp_eq(struct sh_fpu_soft_struct *fregs, struct pt_regs *regs, int m, int n){return 0;}
 
 Link binding: link will be bound to function, not to directive
-// @wi COMP-553 link comment
+// @wi QAZ-76 link comment
 #define XFn  (FR[BANK(XREG(n))])
                
 static int fcmp_eq(struct sh_fpu_soft_struct *fregs, struct pt_regs *regs, int m, int n){return 0;}
